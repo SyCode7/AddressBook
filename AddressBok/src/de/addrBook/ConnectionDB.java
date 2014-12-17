1ff7acbf419e53a64666d47211bfee162ca6b31c
Kennedy <ktorkura@yahoo.com>
2014-12-17 14:59:05 +0100

    derby DB

---
+++ AddressBok/src/de/addrBook/ConnectionDB.java
@@ -0,0 +1,9 @@
+package de.addrBook;
+
+public class ConnectionDB {
+	
+	private static String dbURL = "jdbc:derby://localhost:1527/myDB;create=true;user=me;password=mine";
+	private static String personTable =  "Person";
+	private static String addressTable =  "Address";
+
+}
